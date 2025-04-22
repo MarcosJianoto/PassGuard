96da0e5e5dddd8c4f991e8ac378cef9d41a42706
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2025-04-22 15:30:20 -0300

    first commit - criação do projeto + criação do banco finalizada.

---
+++ src/test/java/com/passguard/PassGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassGuardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
